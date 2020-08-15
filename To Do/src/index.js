ef27c2d4bb01e701c43bb0c4a00e58f2649d2d82
Aritra Kar <aritrakar2002@gmail.com>
2020-08-15 22:01:23 +0530

    Update index.js

--- To Do/src/index.js
+++ To Do/src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import App2 from './App2';
-import App3 from './App3';
-import App4 from './App4';
-import App5 from './App5.js'
 
 ReactDOM.render(
-  <App5 />
+  <App />
   , document.getElementById('root')
 );
 
